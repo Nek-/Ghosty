7741ba941a219a6d49bda26eb5a1630d0e0219e3
Nek <nek.dev@gmail.com>
2012-12-13 00:13:19 +0100

    Started files management

---
+++ src/ghosty/files/Manager.java
@@ -0,0 +1,14 @@
+package ghosty.files;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Manager {
+	public List<String> getFilesFromPath(String path) {
+		return new LinkedList<String>();
+	}
+	
+	public String getFileContent(String path) {
+		return "";
+	}
+}
