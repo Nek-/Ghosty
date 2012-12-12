8ff8a0529770ec550c0242aedb8970c04071dde7
Nek <nek.dev@gmail.com>
2012-12-13 00:11:35 +0100

    Started config management

---
+++ src/ghosty/config/Loader.java
@@ -0,0 +1,15 @@
+package ghosty.config;
+
+import java.util.HashMap;
+
+public class Loader {
+	
+	/**
+	 * Load a file configuration to set it in a hashmap
+	 * @param path
+	 * @return
+	 */
+	public HashMap<String, String> load(String path) {
+		return new HashMap<String, String>();
+	}
+}
---
+++ src/ghosty/config/Manager.java
@@ -0,0 +1,65 @@
+package ghosty.config;
+
+import java.util.HashMap;
+
+public class Manager {
+	
+	/**
+	 * Store the configuration directory
+	 */
+	private String configDir;
+	
+	/**
+	 * Help for loading the configuration from the file
+	 */
+	private Loader loader;
+	
+	/**
+	 * Store all the configuration
+	 */
+	private HashMap<String, String> config;
+	
+	/**
+	 * Construct the configuration manager
+	 * 
+	 * @param path
+	 */
+	public Manager(String path) {
+		this.configDir = path;
+		this.loader = new Loader();
+		
+		if (this.exists())
+			this.load();
+		else
+			this.create();
+	}
+	
+	public String get(String option) {
+		return this.config.get(option);
+	}
+	
+	
+	/**
+	 * Check if the configDir/config.txt exists
+	 * 
+	 * @return
+	 */
+	private boolean exists() {
+		return false;
+	}
+	
+	/**
+	 * Load the configuration,
+	 * simply use the loader
+	 */
+	private void load() {
+		this.config = this.loader.load(this.configDir + "/config.txt");
+	}
+	
+	/**
+	 * Create a new configuration from nothing
+	 */
+	private void create() {
+		
+	}
+}
