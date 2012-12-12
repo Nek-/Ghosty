c1ecd8b5a1b0666faea2b7088b8d193269d9b017
Nek <nek.dev@gmail.com>
2012-12-13 00:13:51 +0100

    Started security management

---
+++ src/ghosty/security/Decryptor.java
@@ -0,0 +1,13 @@
+package ghosty.security;
+
+public class Decryptor {
+	
+	
+	/**
+	 * 
+	 * @param key passphrase
+	 */
+	public Decryptor(String key) {
+		
+	}
+}
---
+++ src/ghosty/security/Encryptor.java
@@ -0,0 +1,14 @@
+package ghosty.security;
+
+public class Encryptor {
+	
+	private String passphrase;
+	
+	/**
+	 * 
+	 * @param key passphrase
+	 */
+	public Encryptor(String key) {
+		
+	}
+}
