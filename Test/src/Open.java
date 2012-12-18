532e1ff913578aff3c7cc9337202206be972e1af
Nekyle <miel.nicolas@gmail.com>
2012-12-18 23:36:32 +0100

    Ca c'est fait

--- Test/src/Open.java
+++ Test/src/Open.java
@@ -1,36 +1,72 @@
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import java.util.Iterator;
+import java.util.List;
 
 public class Open {
-	 public static void main(String[] args) {
+	
+	public static void testDirectoryStream(String arg) throws IOException {
+		
+		Path jdkPath = Paths.get(arg);
+		
+		DirectoryStream<Path> stream = Files.newDirectoryStream(jdkPath);
+		
+		try {
+		
+		Iterator<Path> iterator = stream.iterator();
+		
+		while(iterator.hasNext()) {
+		
+				
+		Path p = iterator.next();
+		System.out.println(p);
+		if ( Files.isDirectory(p))
+			testDirectoryStream( p.toString());
+		
+		}
+		
+		} finally {
+		
+		stream.close();
+		
+		}
+		
+		}
+	
+	public static byte[] getFileContent(String stg) throws IOException {
+		byte[] array = Files.readAllBytes(FileSystems.getDefault().getPath(stg));
+
+		return array;
+	}
+	
+	public static List<String> readFile (String stg) throws IOException{
+		List<String> lignes =  Files.readAllLines(  
+				  FileSystems.getDefault().getPath(stg), Charset.defaultCharset());  
+				
+		return lignes;
+	}
+	
+	 public static void main (String[] args) throws IOException {
 		 Path path = Paths.get("src");
 		 System.out.println("Chemin absolu du fichier : " + path.toAbsolutePath());
 		 System.out.println("Est-ce qu'il existe ? " + Files.exists(path));
 		 System.out.println("Nom du fichier : " + path.getFileName());
 		 System.out.println("Est-ce un répertoire ? " + Files.isDirectory(path));
 			
-		    System.out.println("Affichage des lecteurs à la racine du PC : ");
-		  //On récupère maintenant la liste des répertoires dans une collection typée
-		  //Via l'objet FileSystem qui représente le système de fichier de l'OS hébergeant la JVM
-		  Iterable<Path> roots = FileSystems.getDefault().getRootDirectories();
-
-		  //Maintenant, il ne nous reste plus qu'à parcourir
-		  for(Path chemin : roots){
-		    System.out.println(chemin);
-		    //Pour lister un répertoire, il faut utiliser l'objet DirectoryStream
-		    //L'objet Files permet de créer ce type d'objet afin de pouvoir l'utiliser
-		    try(DirectoryStream<Path> listing = Files.newDirectoryStream(chemin)){
-		  	
-		      int i = 0;
-		      for(Path nom : listing){
-		        System.out.print("\t\t" + ((Files.isDirectory(nom)) ? nom+"/" : nom));
-		        i++;
-		        if(i%4 == 0)System.out.println("\n");
-		      }
-		  		
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }
-		  }
+		 try {
+			testDirectoryStream("C:/Users/Niko/workspace/Test");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 byte[] element = getFileContent("src/Open.java");
+		 for (byte array : element)
+			  System.out.println(array);
+		List<String> lignes = readFile( "src/Open.java");
+		 for (String ligne : lignes)
+			  System.out.println(ligne);
 	 }
 }
--- src/ghosty/files/Manager.java
+++ src/ghosty/files/Manager.java
@@ -1,14 +1,59 @@
 package ghosty.files;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
+
 
 public class Manager {
-	public List<String> getFilesFromPath(String path) {
+	public LinkedList<String> getFilesFromPath(String path)  throws IOException {
+		
+		LinkedList<String> list = new LinkedList<String>();
+		Path jdkPath = Paths.get(path);
+		DirectoryStream<Path> stream = Files.newDirectoryStream(jdkPath);
+		try {
+		
+		Iterator<Path> iterator = stream.iterator();
+		
+		while(iterator.hasNext()) {
+		
+			Path p = iterator.next();
+			
+			if (!p.toString().equals(".ghosty")){
+				list.add(p.toString());
+				if (Files.isDirectory(p))
+					getFilesFromPath(p.toString());
+				}
+		
+		
+			}
+		
+		} finally {
+		
+		stream.close();
+		
+		}
 		return new LinkedList<String>();
 	}
 	
-	public String getFileContent(String path) {
-		return "";
+	public Byte[] getFileContent(Path path) {
+		Byte[] array = null;
+		try {
+			Files.readAllBytes(path);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return array;
 	}
+	
+	public LinkedList readFile ( Path path){
+		
+	}
+	
 }
