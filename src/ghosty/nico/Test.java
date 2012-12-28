79d5e93384a166e1f5bdddfdfe01b0eeb1120515
Nekyle <miel.nicolas@gmail.com>
2012-12-28 23:54:02 +0100

    Classe utile

--- src/ghosty/nico/Test.java
+++ src/ghosty/nico/Test.java
@@ -1,15 +1,135 @@
 package ghosty.nico;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.Drive.Builder;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.*;
+
+
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+//import java.nio.file.Files;
+import java.util.Arrays;
 
 
-public class Test{
+public class Test {
 
-public static void testCreateDirectory() throws IOException {
-	Path monRepertoire = Paths.get("C:/temp/mon_repertoire");
-	Path file = Files.createDirectory(monRepertoire);
-}
+  // ...
+
+  /**
+   * Insert new file.
+   *
+   * @param service Drive API service instance.
+   * @param title Title of the file to insert, including the extension.
+   * @param description Description of the file to insert.
+   * @param parentId Optional parent folder's ID.
+   * @param mimeType MIME type of the file to insert.
+   * @param filename Filename of the file to insert.
+   * @return Inserted file metadata if successful, {@code null} otherwise.
+   */
+  private static File insertFile(Drive service, String title, String description,
+      String parentId, String mimeType, String filename) {
+    // File's metadata.
+    File body = new File();
+    body.setTitle(title);
+    body.setDescription(description);
+    body.setMimeType(mimeType);
+
+    // Set the parent folder.
+    if (parentId != null && parentId.length() > 0) {
+      body.setParents(
+          Arrays.asList(new ParentReference().setId(parentId)));
+    }
+
+    // File's content.
+    java.io.File fileContent = new java.io.File(filename);
+    FileContent mediaContent = new FileContent(mimeType, fileContent);
+    try {
+      File file = service.files().insert(body, mediaContent).execute();
+
+      // Uncomment the following line to print the File ID.
+      // System.out.println("File ID: %s" + file.getId());
+
+      return file;
+    } catch (IOException e) {
+      System.out.println("An error occured: " + e);
+      return null;
+    }
+  }
+  
+  
+  /**
+   * Print a file's metadata.
+   *
+   * @param service Drive API service instance.
+   * @param fileId ID of the file to print metadata for.
+   */
+  private static void printFile(Drive service, String fileId) {
+
+    try {
+      File file = service.files().get(fileId).execute();
+
+      System.out.println("Title: " + file.getTitle());
+      System.out.println("Description: " + file.getDescription());
+      System.out.println("MIME type: " + file.getMimeType());
+    } catch (IOException e) {
+      System.out.println("An error occured: " + e);
+    }
+  }
+
+  /**
+   * Download a file's content.
+   *
+   * @param service Drive API service instance.
+   * @param file Drive File instance.
+   * @return InputStream containing the file's content if successful,
+   *         {@code null} otherwise.
+   */
+  private static InputStream downloadFile(Drive service, File file) {
+    if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
+      try {
+        HttpResponse resp =
+            service.getRequestFactory().buildGetRequest(new GenericUrl(file.getDownloadUrl()))
+                .execute();
+        return resp.getContent();
+      } catch (IOException e) {
+        // An error occurred.
+        e.printStackTrace();
+        return null;
+      }
+    } else {
+      // The file doesn't have any content stored on Drive.
+      return null;
+    }
+  }
+  
+  public static void main(String[] args) throws IOException{
+	  HttpTransport httpTransport = new NetHttpTransport();
+	    JsonFactory jsonFactory = new JacksonFactory();
+	    String CLIENT_ID = "992264517523.apps.googleusercontent.com";
+	    String CLIENT_SECRET = "mKIP4ka2grpm2t67oQQqa7GZ";
+	    String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+	    
+	    String path = "bibi.txt";
+	    Path jdkPath = Paths.get(path);
+	    DirectoryStream<Path> stream = Files.newDirectoryStream(jdkPath);
+	   
+	   Drive service = new Drive.Builder(httpTransport, jsonFactory, null).build();
+	  
+	  
+  }
 }
