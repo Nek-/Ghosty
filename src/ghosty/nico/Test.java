323d8991d2a916e2554ce2030eeaa55030bc1104
Nekyle <miel.nicolas@gmail.com>
2012-12-29 20:12:02 +0100

    Drive

--- src/ghosty/nico/Test.java
+++ src/ghosty/nico/Test.java
@@ -1,6 +1,7 @@
 package ghosty.nico;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.GenericUrl;
@@ -9,21 +10,24 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.drive.Drive.*;
 import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.Drive.Builder;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.*;
 
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 //import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Iterator;
 
 
 public class Test {
@@ -56,8 +60,9 @@ public class Test {
     }
 
     // File's content.
-    java.io.File fileContent = new java.io.File(filename);
-    FileContent mediaContent = new FileContent(mimeType, fileContent);
+    Path fileContent = Paths.get(filename);
+    
+    FileContent mediaContent = new FileContent(mimeType, fileContent.toFile());
     try {
       File file = service.files().insert(body, mediaContent).execute();
 
@@ -70,6 +75,29 @@ public class Test {
       return null;
     }
   }
+  
+  
+  private static File insertDirectory(Drive service, String title, String description,
+	      String parentId, String mimeType, String filename) {
+	    // File's metadata.
+	  File body = new File();
+	    body.setTitle(title);
+	    body.setDescription(description);
+	    body.setMimeType(mimeType);
+	    body.get(filename);
+	    // File's content.
+	   
+	    try {
+	     File parent = service.files().insert(body).execute();
+	      // Uncomment the following line to print the File ID.
+	      // System.out.println("File ID: %s" + file.getId());
+
+	      return parent;
+	    } catch (IOException e) {
+	      System.out.println("An error occured: " + e);
+	      return null;
+	    }
+	  }
   
   
   /**
@@ -117,19 +145,59 @@ public class Test {
     }
   }
   
+ 
+  
   public static void main(String[] args) throws IOException{
-	  HttpTransport httpTransport = new NetHttpTransport();
-	    JsonFactory jsonFactory = new JacksonFactory();
 	    String CLIENT_ID = "992264517523.apps.googleusercontent.com";
 	    String CLIENT_SECRET = "mKIP4ka2grpm2t67oQQqa7GZ";
 	    String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 	    
-	    String path = "bibi.txt";
-	    Path jdkPath = Paths.get(path);
-	    DirectoryStream<Path> stream = Files.newDirectoryStream(jdkPath);
+	    HttpTransport httpTransport = new NetHttpTransport();
+	    JsonFactory jsonFactory = new JacksonFactory();
 	   
-	   Drive service = new Drive.Builder(httpTransport, jsonFactory, null).build();
+	    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+	        httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
+	        .setAccessType("online")
+	        .setApprovalPrompt("auto").build();
+	    
+	    String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
+	    System.out.println("Please open the following URL in your browser then type the authorization code:");
+	    System.out.println("  " + url);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    String code = br.readLine();
+	    
+	    GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
+	    GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
+	    
+	    //Create a new authorized API client
+	    Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
 	  
+	  
+	  Path path = Paths.get("before_coding");
+	  
+	  DirectoryStream<Path> stream = Files.newDirectoryStream(path);
+	  
+	  try {
+	 
+		  Iterator<Path> iterator = stream.iterator();
+		 
+		  while(iterator.hasNext()) {
+			  
+			  Path p = iterator.next();
+			 
+			  if ( Files.isDirectory(p)){
+				   System.out.println(p.getFileName().toString());
+				   insertDirectory(service,  p.getFileName().toString(), "description", null,  "application/vnd.google-apps.folder", p.getFileName().toString());
+			  }
+			  else 
+				  insertFile(service, "title", "description", null,"text/plain", p.getFileName().toString());
+		  }
+	 
+	  } finally {
+	 
+	  stream.close();
+	 
+	  }
 	  
   }
 }
