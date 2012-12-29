f4935512c4df1edfb8f03b225298b41dbc41aa54
Nek <nek.dev@gmail.com>
2012-12-29 17:43:42 +0100

    Added create directory function

--- src/ghosty/files/Directory.java
+++ src/ghosty/files/Directory.java
@@ -1,5 +1,9 @@
 package ghosty.files;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 
 /**
@@ -15,5 +19,10 @@ public class Directory {
 	
 	public LinkedList<String> getFiles () {
 		return new LinkedList<String>();
+	}
+	
+	public static void testCreateDirectory(String directory) throws IOException {
+		Path monRepertoire = Paths.get(directory);
+		Path file = Files.createDirectory(monRepertoire);
 	}
 }
