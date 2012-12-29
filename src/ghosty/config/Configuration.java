cf7ca339cf3b7b83ee78ce9b365f0c63ac1751b9
Nek <nek.dev@gmail.com>
2012-12-29 17:44:28 +0100

    Updated configuration

--- src/ghosty/config/Configuration.java
+++ src/ghosty/config/Configuration.java
@@ -3,6 +3,9 @@ package ghosty.config;
 import ghosty.config.loader.ConfigLoaderInterface;
 import ghosty.config.loader.LoadException;
 import ghosty.config.loader.XmlConfigLoader;
+import ghosty.config.saver.ConfigSaverInterface;
+import ghosty.config.saver.SaveException;
+import ghosty.config.saver.XmlConfigSaver;
 import ghosty.config.utils.ConfigMapList;
 
 import java.util.Iterator;
@@ -25,21 +28,31 @@ public class Configuration implements Iterable {
 	private ConfigMapList config;
 	
 	/**
+	 * Help for save the configuration to a file
+	 */
+	private ConfigSaverInterface saver;
+	
+	/**
 	 * Construct the configuration manager
 	 * 
 	 * @param path
 	 * @throws LoadException 
 	 */
 	public Configuration(String path) throws LoadException {
-		this.path = path;
+		this.path   = path;
 		this.loader = new XmlConfigLoader();
+		this.saver  = new XmlConfigSaver();
 
-		if (this.exists())
-			this.load();
-		else
-			this.create();
+		this.load();
 	}
 	
+	/**
+	 * Get an option from the loaded configuration
+	 * 
+	 * @param option
+	 * @return
+	 * @throws MissingConfigurationException
+	 */
 	public String get(String option) throws MissingConfigurationException {
 		if(this.config.containsKey(option)){
 
@@ -48,18 +61,32 @@ public class Configuration implements Iterable {
 		throw new MissingConfigurationException("The option " + option + " do not exists in the configuration file " + this.path + ".");
 	}
 	
+	/**
+	 * Set an option on the configuration
+	 * 
+	 * @param option
+	 * @param value
+	 */
+	public void set(String option, String value) {
+		this.config.put(option, value);
+	}
+	
+	public boolean has(String option) {
+		return this.config.containsKey(option);
+	}
+	
 	
 	/**
 	 * Check if the configDir/config.txt exists
 	 * 
 	 * @return
 	 */
-	private boolean exists() {
+	public boolean exists() {
 		return !this.config.isEmpty();
 	}
 	
 	/**
-	 * Load the configuration,
+	 * Load the configuration
 	 * simply use the loader
 	 * @throws LoadException 
 	 */
@@ -67,12 +94,10 @@ public class Configuration implements Iterable {
 		this.config = this.loader.load(this.path);
 	}
 	
-	/**
-	 * Create a new configuration from nothing
-	 */
-	private void create() {
-		
+	public void save() throws SaveException {
+		this.saver.save(this.path, this.config);
 	}
+	
 
 	@Override
 	public Iterator iterator() {
