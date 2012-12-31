f66167d8e80df6cb1190b88c6b0fd276e659133a
Nek <nek.dev+git@gmail.com>
2012-12-31 14:15:33 +0100

    Make watcher work

--- src/ghosty/Main.java
+++ src/ghosty/Main.java
@@ -1,10 +1,17 @@
 package ghosty;
 
+import java.io.IOException;
 import java.nio.file.FileSystems;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.List;
 import java.util.Scanner;
+import static java.nio.file.StandardWatchEventKinds.*;
 
 import ghosty.config.Configuration;
 import ghosty.files.Directory;
@@ -12,6 +19,8 @@ import ghosty.utils.Parameters;
 import ghosty.utils.ParametersManager;
 
 import javax.swing.SwingUtilities;
+
+import com.google.api.services.drive.model.File;
 
 /**
  * Main class of the project Ghost
@@ -68,7 +77,7 @@ public class Main {
 		
 		Directory.createDirectory(baseConfigDirectory);
 		String lastDirectory = null;
-		String directory = null;
+		String directory = "";
 		try {
 			Configuration baseConfig = new Configuration(baseConfigDirectory +  System.getProperty("file.separator") + "base.xml");
 			if (baseConfig.exists() && baseConfig.has("last_directory")) {
@@ -102,7 +111,7 @@ public class Main {
 		
 		// Watcher
 		System.out.println("Pour arrêter ce programme, tapez CTRL+C.");
-		launchWatcher();
+		launchWatcher(Paths.get(directory));
 
 
 		// If configuration missing or reconfigure enabled, we must load the configuration process
@@ -111,70 +120,49 @@ public class Main {
 
 
 	private static void launchWatcher(Path dir) {
-		
-		WatchService watcher = FileSystems.getDefault().newWatchService();
-		
+
 		for (;;) {
 
-		    // wait for key to be signaled
-		    WatchKey key;
-		    try {
-		        key = watcher.take();
-		    } catch (InterruptedException x) {
-		        return;
-		    }
+			try {
+				WatchService watcher = dir.getFileSystem().newWatchService();
+				dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
 
-		    for (WatchEvent<?> event: key.pollEvents()) {
-		        WatchEvent.Kind<?> kind = event.kind();
+				WatchKey watckKey = watcher.take();
 
-		        // This key is registered only
-		        // for ENTRY_CREATE events,
-		        // but an OVERFLOW event can
-		        // occur regardless if events
-		        // are lost or discarded.
-		        if (kind == OVERFLOW) {
-		            continue;
-		        }
+				List<WatchEvent<?>> events = watckKey.pollEvents();
+				
+				// TODO : added action on events (replace entry)
+				for (WatchEvent event : events) {
+					if (event.kind() == OVERFLOW) {
+						continue;
+					}
 
-		        // The filename is the
-		        // context of the event.
-		        WatchEvent<Path> ev = (WatchEvent<Path>)event;
-		        Path filename = ev.context();
+					if (event.kind() == ENTRY_CREATE) {
+						System.out.println("Created: "
+								+ event.context().toString());
+					}
+					if (event.kind() == ENTRY_DELETE) {
+						System.out.println("Delete: "
+								+ event.context().toString());
+					}
+					if (event.kind() == ENTRY_MODIFY) {
+						System.out.println("Modify: "
+								+ event.context().toString());
+					}
 
-		        // Verify that the new
-		        //  file is a text file.
-		        try {
-		            // Resolve the filename against the directory.
-		            // If the filename is "test" and the directory is "foo",
-		            // the resolved name is "test/foo".
-		            Path child = dir.resolve(filename);
-		            if (!Files.probeContentType(child).equals("text/plain")) {
-		                System.err.format("New file '%s'" +
-		                    " is not a plain text file.%n", filename);
-		                continue;
-		            }
-		        } catch (IOException x) {
-		            System.err.println(x);
-		            continue;
-		        }
+				}
+				boolean valid = watckKey.reset();
+				if (!valid) {
+					System.out
+							.println("A critical error appears: the directory is not enough accessible");
+					break;
+				}
 
-		        // Email the file to the
-		        //  specified email alias.
-		        System.out.format("Emailing file %s%n", filename);
-		        //Details left to reader....
-		    }
-
-		    // Reset the key -- this step is critical if you want to
-		    // receive further watch events.  If the key is no longer valid,
-		    // the directory is inaccessible so exit the loop.
-		    boolean valid = key.reset();
-		    if (!valid) {
-		    	System.out.println("A critical error appears: the directory is not enough accessible");
-		        break;
-		    }
+			} catch (Exception e) {
+				System.out.println("Error: " + e.toString());
+			}
 		}
-		// TODO Auto-generated method stub
-		
+
 	}
 	
 	
